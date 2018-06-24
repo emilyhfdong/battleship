c161f68790a8dc21999e363e50f6cd55e614f982
Emily Dong <emilyhfdong@gmail.com>
2018-06-24 22:06:54 +0000

    sass and input form

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -12,9 +12,9 @@ $(document).ready(function(){
     let $box = $("<div class='box grey-box'></div>");
 
     for (let i = 0; i < 10; i ++ ) {
-      let $row = $(`<div class='row yVal${i}'></div>`);
+      let $row = $(`<div class='row'></div>`);
       for (let j = 0; j < 10; j ++ ) {
-        $row.append($box.clone().addClass(`xVal${j}`));
+        $row.append($box.clone().attr("data-x", j).attr("data-y", i));
       }
       $(gridClass).append($row)
     }
@@ -23,39 +23,105 @@ $(document).ready(function(){
   createGrid(".my-grid");
   createGrid(".enemy-grid");
 
+
+
   function makeShips (shipName) {
     let $box = $("<div class='box'></div>");
-    let $ship = $(`<div class='ship'></div>`);
+    let $ship = $(`
+      <div class='ship'>
+        <textarea class="xPos" placeholder="x"></textarea>
+        <textarea class="yPos" placeholder="y"></textarea>
+        <button class="go">
+          <p>place ship</p>
+        </button>
+      </div>`);
     for (let i = 0; i < shipLengths[shipName]; i ++ ) {
-      $ship.append($box.clone().addClass(shipName));
+      $ship.append($box.clone().addClass(shipName)).attr("shipName", shipName);
     }
+
 
     return $ship;
   }
 
   function loadShips () {
     for (ship in shipLengths) {
-      $(".grid-container .all-ships").append(makeShips(ship));
+      $errors = $("<div class='errors'></div>")
+      $(".grid-container .all-ships").append(makeShips(ship)).append($errors);
     }
   }
 
   loadShips();
 
+  // when ship is double clicked, it rotates 90 deg
   $(".all-ships .ship").dblclick(function(ev) {
     $(this).toggleClass("vertical-ship");
   });
 
-
+  // when START GAME is clicked, enemgy grid appears and all-ships disappears
   $("header .start-game").on("click", function(ev) {
     $(".grid-container .enemy-grid").removeClass("hidden");
     $(".grid-container .all-ships").addClass("hidden");
   });
 
+  // when NEW GAME is clicked, enemy grid disappears and all-ships appears
   $("header .new-game").on("click", function(ev) {
     $(".grid-container .enemy-grid").addClass("hidden");
     $(".grid-container .all-ships").removeClass("hidden");
   });
 
+  // when a position is inputted...
+  $(".ship .go").on("click", function () {
+    let $goButton = $(this);
+    let $ship = $goButton.parent();
+    let shipName = $ship.attr("shipName")
+    let $errors = $goButton.parent().next();
+    let numberOfErrors = 0;
+
+    $errors.empty();
+
+    let xPosition = $goButton.siblings('.xPos').val();
+    let yPosition = $goButton.siblings('.yPos').val();
+
+    if (yPosition.toString() === "") {
+      $errors.append("<p>please input a y position</p>");
+      numberOfErrors += 1;
+    }
+    if (xPosition.toString() === "") {
+      $errors.append("<p>please input a x position</p>");
+      numberOfErrors += 1;
+    }
+    if ($ship.hasClass("vertical-ship")) {
+      if ((Number(yPosition) + shipLengths[shipName]) > 9 ) {
+        for (let i = 0; i < (10 - Number(yPosition)); i ++ ) {
+          $(`.grid [data-x="${Number(xPosition)}"][data-y="${Number(yPosition) + i}"]`).addClass(`${shipName} lighter`);
+        }
+        $errors.append("<p>ship does not fit</p>");
+        numberOfErrors += 1;
+      }
+    } else {
+      if ((Number(xPosition) + shipLengths[shipName]) > 9 ) {
+        for (let i = 0; i < (10 - Number(xPosition)); i ++ ) {
+          $(`.grid [data-x="${Number(xPosition) + i}"][data-y="${Number(yPosition)}"]`).addClass(`${shipName} lighter`);
+        }
+        $errors.append("<p>ship does not fit</p>");
+        numberOfErrors += 1;
+      }
+    }
+
+    if (numberOfErrors === 0) {
+      $ship.remove();
+      if ($ship.hasClass("vertical-ship")) {
+        for (let i = 0; i < shipLengths[shipName]; i ++ ) {
+          $(`.grid [data-x="${Number(xPosition)}"][data-y="${Number(yPosition) + i}"]`).addClass(shipName);
+        }
+      } else {
+        for (let i = 0; i < shipLengths[shipName]; i ++ ) {
+          $(`.grid [data-x="${Number(xPosition) + i}"][data-y="${Number(yPosition)}"]`).addClass(shipName);
+        }
+      }
+    }
+
+  });
 
 });
 
--- server.js
+++ server.js
@@ -2,10 +2,19 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const PORT = 8080; // default port 8080
+const nodeSassMiddleware = require('node-sass-middleware');
+const path = require('path');
 
 const shipFunctions = require("./shipFunctions.js");
 
-app.use(express.static("public"));
+app.use(nodeSassMiddleware({
+    src: path.join(__dirname, 'sass'),
+    dest: path.join(__dirname, 'public'),
+    debug: true,
+    outputStyle: 'compressed'
+}));
+
+app.use(express.static('public'));
 app.use(bodyParser.json());
 
 
